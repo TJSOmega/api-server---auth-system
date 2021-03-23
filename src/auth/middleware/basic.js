f4224466f9bf2d25337cbb8b2aaa5beee71ff873
Terrence Jamal Simms <Tjs@omegacc.com>
2021-03-22 18:42:41 -0700

    Finished auth routes

---
+++ src/auth/middleware/basic.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const base64 = require('base-64');
+const User = require('../models/users.js');
+
+module.exports = async (req, res, next) => {
+  if (!req.headers.authorization) {return _authError(); }
+
+  let basic = req.headers.authorization.split(' ').pop();
+  let [username, password] = base64.decode(basic).split(':');
+
+  try {
+    req.user = await User.authenticateBasic(username, password)
+    next()
+  } catch (e) {
+    _authError()
+  }
+
+  function _authError() {
+    res.status(403).send('Invalid Login');
+  }
+}
