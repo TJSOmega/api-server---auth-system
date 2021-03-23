7fde9909c6fcec3d0afc04a071d74e452ead635e
Dawit Ayana <dawitz28@gmail.com>
2021-03-22 18:49:51 -0700

    auth-system

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/food', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const server = require('./src/server.js');
+
+server.start(3000);
